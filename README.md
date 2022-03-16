60137ff1d0eefa80ae6fec1e43d94a06a3ab78f0
Luciano da Silva Ribas <lucribas@users.noreply.github.com>
2022-03-15 21:30:18 -0300

    improved readme and minor fixes

--- README.md
+++ README.md
@@ -1,13 +1,107 @@
 # go-jira-migrate
-a golang tool to migrate jira project
 
-Read jira issues from SRC, apply some transformations like google translate and create issues on DST.
+**A golang tool to migrate Jira issues from one server to another.**
+
+## How it works
+
+1. For each issue in `Jira.csv`:
+1.1. Connect to `Jira source server` and get all details of issue.
+1.2. Use *Google Translate API* to translate the *Summary* and *Description* from `pt-br` to `en`.
+1.3. Fix the *IssueType* to the type available on `Jira destination server`.
+1.4. Copy custom fields like *Story Points*.
+1.5. Copy *Labels* field.
+1.6. Add a label with the *Key of source issue*. That help you to find one issue using the source Key.
+1.7. Add a label with the *name of Components of source issue*. Since the destination server have other components we add them as labels to preserve information.
+1.8. Create a new issue on `Jira destination server`.
+
+## Setup
+
+Generate a list of issues using a `Jira SQL query` like:
+```sql
+project = MSS AND Sprint in futureSprints() ORDER BY Key ASC
+```
+Export the issues to a *csv file* as `Jira.csv`.
+
+Create two *Jira configuration files* as bellow:
+
+`secrets_src.json`
+```json
+{
+	"JiraUrl": "https://your_src_jira_domain.atlassian.net",
+	"JiraUserName":"your_id@mmm.com",
+	"JiraUserToken":"dlfhsaldflsdafg9245"
+}
+```
+
+`secrets_dst.json`
+```json
+{
+	"JiraUrl": "https://your_dst_jira_domain.atlassian.net",
+	"JiraUserName":"your_id@mmm.com",
+	"JiraUserToken":"779776869796gjvui6giu6n"
+}
+```
+Notes:
+- For atlassian.net cloud create a token and use it.
+- For on premisse use your password on JiraUserToken.
+
+Check and modify the function createFromIssue() on migrate.go to apply Issue transformations that you need.
+
+*Tip: you can get the complete issue structure of your server just by downloading the json from an Issue.Self URL and using vscode to format it*
+
 
 # install
 
+```bash
 go get github.com/bregydoc/gtranslate
 go mod init test
 go mod tidy
+```
 
+
+
+# run
+
+```bash
 go run migrate.go
+```
 
+# example
+
+Issue **read** from source server:
+```text
+** Processing Issue:  MSS-216
+Self :https://xxxxxxxxx.atlassian.net/rest/api/2/issue/90980
+Key :MSS-216
+Summary: [feature] Estudo e documentação para captura de métricas de utilização do app - Parte 1
+Type: Story
+Priority: Major
+Description: Pesquisar soluções de codigo aberto para a captura de métricas de uso do App.
+E Documentar.
+Se houver mais do que um comparar os recursos para seleção.
+Rodar em timebox até onde couber dentro de 5 ptos (1 semana).
+a refencia é o Firebase (não é codigo aberto).
+```
+
+Issue **written** to destination server:
+```text
+CREATED:Self :https://xxxx.com/rest/api/2/issue/1627192
+Key :DMOB-1364
+Summary: [feature] Study and documentation to capture app usage metrics - Part 1
+Type: Story
+Priority: Lowest
+Description: Research open source solutions for capturing App usage metrics.
+And Document.
+If there is more than one compare the features for selection.
+Run in timebox as far as it will fit within 5 points (1 week).
+the reference is Firebase (not open source).
+----
+*[pt-br original]*
+Pesquisar soluções de codigo aberto para a captura de métricas de uso do App.
+E Documentar.
+Se houver mais do que um comparar os recursos para seleção.
+Rodar em timebox até onde couber dentro de 5 ptos (1 semana).
+a refencia é o Firebase (não é codigo aberto).
+
+*Imported* from Jira Certi: https://xxxxxxxxx.atlassian.net/browse/MSS-216
+```
---
+++ migrate.go
@@ -0,0 +1,190 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+
+	"github.com/andygrunwald/go-jira"
+	"github.com/bregydoc/gtranslate"
+	"github.com/gocarina/gocsv"
+	"github.com/tkanos/gonfig"
+	"github.com/trivago/tgo/tcontainer"
+)
+
+type Configuration struct {
+	JiraUrl       string
+	JiraUserName  string
+	JiraUserToken string
+}
+
+type CsvIssue struct {
+	Key string `csv:"Issue_key"`
+}
+
+func main() {
+
+	// open Jira Connections
+	jiraClientSrc := openJira("secrets_src.json")
+	jiraClientDst := openJira("secrets_dst.json")
+
+	// // Initial Test
+	// issue, _, _ := jiraClientSrc.Issue.Get("MSS-260", nil)
+	// printIssue(issue)
+	// issue, _, _ = jiraClientDst.Issue.Get("DMOB-1202", nil)
+	// printIssue(issue)
+
+	// Read csv
+	issues := readCSV("Jira.csv")
+
+	for _, i := range issues {
+		fmt.Printf("------------------------------\n")
+		fmt.Println("** Processing Issue: ", i.Key)
+		// get Issue from SRC
+		issue, _, _ := jiraClientSrc.Issue.Get(i.Key, nil)
+		// translate issue fields
+		issue.Fields.Summary = translateSummary(issue.Fields.Summary)
+		if issue.Fields.Description != "" {
+			issue.Fields.Description = translateDescription(issue.Fields.Description)
+		}
+		import_message := "\n*Imported* from Jira Certi: https://certi-cdm.atlassian.net/browse/" + issue.Key
+		issue.Fields.Description = issue.Fields.Description + import_message
+		printIssue(issue)
+		// create Issue on DST
+		createFromIssue(jiraClientDst, issue)
+	}
+}
+
+func openJira(conf_file string) *jira.Client {
+
+	configuration := Configuration{}
+	err := gonfig.GetConf(conf_file, &configuration)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Start Jira with username: %s\n", configuration.JiraUserName)
+
+	base := configuration.JiraUrl
+	tp := jira.BasicAuthTransport{
+		Username: configuration.JiraUserName,
+		Password: configuration.JiraUserToken,
+	}
+
+	jiraClient, err := jira.NewClient(tp.Client(), base)
+	if err != nil {
+		panic(err)
+	}
+
+	return jiraClient
+
+}
+
+func readCSV(csv_file string) []*CsvIssue {
+	in, err := os.Open(csv_file)
+	if err != nil {
+		panic(err)
+	}
+	defer in.Close()
+
+	issues := []*CsvIssue{}
+
+	if err := gocsv.UnmarshalFile(in, &issues); err != nil {
+		panic(err)
+	}
+
+	return issues
+}
+
+func printIssue(issue *jira.Issue) {
+	fmt.Printf("Self :%s\n", issue.Self)
+	fmt.Printf("Key :%s\n", issue.Key)
+	fmt.Printf("Summary: %+v\n", issue.Fields.Summary)
+	fmt.Printf("Type: %s\n", issue.Fields.Type.Name)
+	fmt.Printf("Priority: %s\n", issue.Fields.Priority.Name)
+	fmt.Printf("Description: %s\n", issue.Fields.Description)
+	fmt.Printf("Labels: %s\n", issue.Fields.Labels)
+}
+
+func translateSummary(text string) string {
+	translated, err := gtranslate.TranslateWithParams(
+		text,
+		gtranslate.TranslationParams{
+			From: "pt",
+			To:   "en",
+		},
+	)
+	if err != nil {
+		panic(err)
+	}
+
+	return translated
+}
+func translateDescription(text string) string {
+	translated, err := gtranslate.TranslateWithParams(
+		text,
+		gtranslate.TranslationParams{
+			From: "pt",
+			To:   "en",
+		},
+	)
+	if err != nil {
+		panic(err)
+	}
+
+	header := "\n----\n*[pt-br original]*\n"
+	return translated + header + text + "\n"
+}
+
+func createFromIssue(client *jira.Client, issueFrom *jira.Issue) {
+
+	// Story points: import from custom field
+	unknowns := tcontainer.NewMarshalMap()
+	unknowns["customfield_10002"] = issueFrom.Fields.Unknowns["customfield_10002"]
+
+	// Labels: original labels plus compent name
+	labels := issueFrom.Fields.Labels
+	labels = append(labels, issueFrom.Key) // add orign key
+	for _, c := range issueFrom.Fields.Components {
+		labels = append(labels, c.Name)
+	}
+
+	// IssueType mapping
+	issueType := issueFrom.Fields.Type.Name
+	if issueType == "Improvement" {
+		issueType = "New Feature"
+	}
+	if issueType == "Sub-task" {
+		issueType = "Task"
+	}
+
+	i := jira.Issue{
+		Fields: &jira.IssueFields{
+			Assignee: &jira.User{
+				Name: "lribas",
+			},
+			Description: issueFrom.Fields.Description,
+			Type: jira.IssueType{
+				Name: issueType,
+			},
+			Project: jira.Project{
+				Key: "DMOB",
+			},
+			Summary:  issueFrom.Fields.Summary,
+			Unknowns: unknowns,
+			Labels:   labels,
+		},
+	}
+
+	issue, resp, err := client.Issue.Create(&i)
+	if err != nil {
+		body, _ := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			fmt.Printf(string(body))
+		}
+		panic(err)
+	}
+	fmt.Printf("CREATED:")
+	issue, _, _ = client.Issue.Get(issue.Key, nil)
+	printIssue(issue)
+}
